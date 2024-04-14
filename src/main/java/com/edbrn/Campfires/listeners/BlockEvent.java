ec673a04de03efc52946327bf7f9520033d258dc
Jordan Brown <hello@edbrn.com>
2024-04-14 16:02:52 +0100

    Make it so players can place campfire/gold block and a campfire is recorded in the config

--- src/main/java/com/edbrn/Campfires/listeners/BlockEvent.java
+++ src/main/java/com/edbrn/Campfires/listeners/BlockEvent.java
@@ -1,7 +1,9 @@
 package com.edbrn.Campfires.listeners;
 
+import com.edbrn.Campfires.files.CampfiresConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
+import org.bukkit.block.Campfire;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,11 +15,20 @@ public class BlockEvent implements Listener {
     public void onBlockPlace(BlockPlaceEvent event) {
         Block block = event.getBlockPlaced();
         Player player = event.getPlayer();
+
         if (block.getType().name().equals("GOLD_BLOCK")) {
             Block blockAboveThis = player.getWorld().getBlockAt(block.getX(), block.getY() + 1, block.getZ());
-            player.sendMessage(String.format("Block %s above", blockAboveThis.getType().toString()));
             if (blockAboveThis.getType().name().equals("CAMPFIRE")) {
-                player.sendMessage("Campfire ontop of gold");
+                new CampfiresConfig(Bukkit.getLogger(), "campfires.json").addCampfire(blockAboveThis.getX(), blockAboveThis.getY(), blockAboveThis.getZ(), player);
+                player.sendMessage("Campfire created.");
+            }
+        }
+
+        if (block.getType().name().equals("CAMPFIRE")) {
+            Block blockBeneathThis = player.getWorld().getBlockAt(block.getX(), block.getY() - 1, block.getZ());
+            if (blockBeneathThis.getType().name().equals("GOLD_BLOCK")) {
+                new CampfiresConfig(Bukkit.getLogger(), "campfires.json").addCampfire(block.getX(), block.getY(), block.getZ(), player);
+                player.sendMessage("Campfire created.");
             }
         }
     }
